e66f31d69ff5bb72a8ba8d15de6cd0e88c4453c8
ThayalanGR <grthayalan18@gmail.com>
2020-12-19 13:51:36 +0530

    core-server base architecture done

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # core-server
-core server is the hear of the sellerspot eco system, it will hold perform opearation such as creating and managing tenants, hold the billing status of the tenant, install and customize application and more.
+
+core server is the heart of the sellerspot eco-system, it will hold perform opearation such as creating and managing tenants, hold the billing status of the tenant, install and customize application and more.
---
+++ webpack.config.ts
@@ -0,0 +1,61 @@
+import path from 'path';
+import webpack, { Configuration } from 'webpack';
+import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import WebpackShellPluginNext from 'webpack-shell-plugin-next';
+import packageJson from './package.json';
+
+const webpackConfiguration = (env: {
+    production: boolean;
+    development: boolean;
+}): Configuration => {
+    const isProduction = env.production ? true : false;
+    return {
+        entry: 'src',
+        resolve: {
+            extensions: ['.ts', '.js'],
+            plugins: [new TsconfigPathsPlugin()],
+        },
+        output: {
+            path: path.join(__dirname, '/dist'),
+            filename: 'index.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true,
+                    },
+                    exclude: [/dist/, /node_modules/],
+                },
+            ],
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': isProduction,
+                'process.env.APP_NAME': JSON.stringify(packageJson.name),
+                'process.env.APP_VERSION': JSON.stringify(packageJson.version),
+            }),
+            new ForkTsCheckerWebpackPlugin({
+                eslint: {
+                    files: 'src',
+                },
+            }),
+            !isProduction
+                ? new WebpackShellPluginNext({
+                      onBuildEnd: {
+                          scripts: ['npm run dev:server'],
+                          blocking: false,
+                          parallel: true,
+                      },
+                      safe: true,
+                  })
+                : new webpack.DefinePlugin({}),
+        ],
+        watch: !isProduction,
+    };
+};
+
+export default webpackConfiguration;
